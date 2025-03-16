152b2433bf5b3497836aa1e7ced3c18df2eedac6
Kevin <kevin@kokev.de>
2025-03-16 20:36:45 +0100

    Test same structure as all other repos

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_jsonnet_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-jsonnet"
+	tree_sitter_jsonnet "github.com/koskev/tree-sitter-jsonnet/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
